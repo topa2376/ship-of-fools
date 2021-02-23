c98008cf27dbb938db36d1ac4ad901f70463ab5e
topa2376 <to@biaz.se>
2021-02-23 16:28:25 +0100

    added login box for VIP members

--- resources/js/control_login_box.js
+++ resources/js/control_login_box.js
@@ -1,25 +1,25 @@
 // Get the order_box
-var order_box = document.getElementById("myorder_box");
+var login_box = document.getElementById("mylogin_box");
 
 // Get the button that opens the order_box
-var btn = document.getElementById("open_order_box");
+var btn = document.getElementById("open_login_box");
 
 // Get the <span> element that closes the order_box
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the order_box
 btn.onclick = function() {
-  order_box.style.display = "block";
+  login_box.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the order_box
 span.onclick = function() {
-  order_box.style.display = "none";
+  login_box.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the order_box, close it
 window.onclick = function(event) {
   if (event.target == order_box) {
-    order_box.style.display = "none";
+    login_box.style.display = "none";
   }
 }
