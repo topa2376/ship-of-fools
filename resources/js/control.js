7d91b10ab2cc0fd821365a5d3bc7554c6185b996
Tingmo Liu <44380153+liti1731@users.noreply.github.com>
2021-02-25 20:38:00 +0100

    VIP profile page update1

--- resources/js/control.js
+++ resources/js/control.js
@@ -1,10 +1,118 @@
-//Switch language
+//============================== Switch language ===============================
 function switchLanguage () {
-  if (language==true){// English
-     document.getElementById("languageId").src="./resources/images/swedish.png";
-     language=!language;
+  if (language=='en'){// English
+     document.getElementById("languageId").src="./resources/img/swedish.png";  
+     language='se';
+     update_language();
   }else{
-    document.getElementById("languageId").src="./resources/images/english.png";
-    language=!language;
+    document.getElementById("languageId").src="./resources/img/english.png";
+    language='en';
+    update_language();
   }// Swedish
 }
+
+function update_language(){
+  keys = lanDict['keys'];
+  for (idx in keys) {
+      key = keys[idx];
+      $("#" + key).text(get_string(key));
+  };
+}
+//==============================
+
+
+
+//============================== Purchase Items ===============================
+function add_purchase(){
+  //get content from page 
+  //perform operation 
+  //update model 
+  //update view
+}
+
+function sub_purchase(){
+  //get content from page 
+  //perform operation 
+  //update model 
+  //update view
+}
+
+function change_quantity(){
+  //get content from page 
+  //perform operation 
+  //update model 
+  //update view
+}
+
+function update_total(result){
+}
+
+
+//============================== Sort and Select Catagories ========================
+function set_categories (){
+  var children = document.getElementById("filterBox").children;
+  for (i = 0; i < children.length; i++){
+    children[i].id = topCategory[i];
+  }
+}
+
+function list_selected(category){
+  var category = category.id;
+  remove_prod(); //clear list
+  var list = selectedCategory(category);
+  var iter = list.length;
+  if (iter > cnst['max_list_item']){ iter = cnst['max_list_item']};
+  for (i = 0; i < iter; i++){
+    drink_box(list[i]);
+  }
+}
+
+function remove_prod(){
+  var myNode = document.getElementById("groupDrinkBox");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);
+  }
+  //removes the prod boxes.
+}
+
+
+//============================== Create item box ========================
+function drink_box(prod) {
+  var dst = "groupDrinkBox";
+  var div = document.createElement('div');
+  var prodDiv = prod.articleid;
+  div.className = 'singleDrinkBox';
+  div.id = prodDiv;
+  var img = document.createElement('img');
+  var name = document.createElement('p');
+  var price = document.createElement('p');
+  img.src = prod.img;
+  name.textContent = prod.name + " (" + prod.alcoholstrength + ")"; 
+  price.textContent = "SEK " + prod.priceinclvat;
+  document.getElementById(dst).appendChild(div);
+  document.getElementById(prodDiv).appendChild(img);
+  document.getElementById(prodDiv).appendChild(name);
+  document.getElementById(prodDiv).appendChild(price);
+}
+
+//============================== Update and set View ========================
+function update_view(){
+  //change the dict elements 
+  //update all the product elements
+}
+
+function intialize_view() {
+  // place all the products 
+  for (i = 0; i < 12; i++){
+    var prod = get_product(i);
+    drink_box(prod);
+  }
+  set_categories ();
+  update_language();
+}
+
+// We don't update the view the first time until the document is ready
+// loading.
+$(document).ready(function() {
+  intialize_view();
+})
--- resources/js/dictionary.js
+++ resources/js/dictionary.js
@@ -1,1 +1,36 @@
-var language=true;
+var language = 'en';
+
+//here is are all the links for the other pages
+dirDict = {             
+    'home': ''
+}
+
+//dictionary for the text and lanaguges
+lanDict = {
+    'keys' : ['login','total'],
+    'en' : {
+        'login': 'Login',
+        'total': 'Total'
+    },
+    'se' : {
+        'login': 'Logga In',
+        'total': 'Totalbelopp'
+    }
+}
+
+var topCategory = ['All', 'Vitt vin', 'Cognac', 'Okryddad sprit', 'Kryddad sprit', 'Whisky, Malt'];
+
+var cnst = {
+    'max_list_item' : 12,
+    'start_val' : 0,
+    'start_res' : 0
+}
+
+
+function get_string(key) {
+    return lanDict[language][key];
+}
+
+function get_product(key){
+    return engBev[key];
+}
