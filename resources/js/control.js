a8f6770723e7c2cabfcd287d695864f1da9ae263
cesnygren <79372859+cesnygren@users.noreply.github.com>
2021-02-25 15:16:32 +0100

    Update control.js

--- resources/js/control.js
+++ resources/js/control.js
@@ -1,4 +1,4 @@
-//Switch language
+//============================== Switch language ===============================
 function switchLanguage () {
   if (language=='en'){// English
      document.getElementById("languageId").src="./resources/img/swedish.png";  
@@ -11,6 +11,18 @@ function switchLanguage () {
   }// Swedish
 }
 
+function update_language(){
+  keys = lanDict['keys'];
+  for (idx in keys) {
+      key = keys[idx];
+      $("#" + key).text(get_string(key));
+  };
+}
+//==============================
+
+
+
+//============================== Purchase Items ===============================
 function add_purchase(){
   //get content from page 
   //perform operation 
@@ -36,32 +48,37 @@ function update_total(result){
 }
 
 
-function update_language(){
-  keys = lanDict['keys'];
-  for (idx in keys) {
-      key = keys[idx];
-      $("#" + key).text(get_string(key));
-  };
-}
-
-function update_view(){
-  //change the dict elements 
-  //update all the product elements
-}
-
-function intialize_view() {
-  //this will have to change
-  var dst = "groupDrinkBox";
-
-  // place all the products 
-  for (i = 0; i < 12; i++){
-    var prod = get_product(i);
-    drink_box(dst, prod);
+//============================== Sort and Select Catagories ========================
+function set_categories (){
+  var children = document.getElementById("filterBox").children;
+  for (i = 0; i < children.length; i++){
+    children[i].id = topCategory[i];
   }
-  update_language();
 }
 
-function drink_box(destination, prod) {
+function list_selected(category){
+  var category = category.id;
+  remove_prod(); //clear list
+  var list = selectedCategory(category);
+  var iter = list.length;
+  if (iter > cnst['max_list_item']){ iter = cnst['max_list_item']};
+  for (i = 0; i < iter; i++){
+    drink_box(list[i]);
+  }
+}
+
+function remove_prod(){
+  var myNode = document.getElementById("groupDrinkBox");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);
+  }
+  //removes the prod boxes.
+}
+
+
+//============================== Create item box ========================
+function drink_box(prod) {
+  var dst = "groupDrinkBox";
   var div = document.createElement('div');
   var prodDiv = prod.articleid;
   div.className = 'singleDrinkBox';
@@ -72,24 +89,30 @@ function drink_box(destination, prod) {
   img.src = prod.img;
   name.textContent = prod.name + " (" + prod.alcoholstrength + ")"; 
   price.textContent = "SEK " + prod.priceinclvat;
-  document.getElementById(destination).appendChild(div);
+  document.getElementById(dst).appendChild(div);
   document.getElementById(prodDiv).appendChild(img);
   document.getElementById(prodDiv).appendChild(name);
   document.getElementById(prodDiv).appendChild(price);
 }
 
+//============================== Update and set View ========================
+function update_view(){
+  //change the dict elements 
+  //update all the product elements
+}
+
+function intialize_view() {
+  // place all the products 
+  for (i = 0; i < 12; i++){
+    var prod = get_product(i);
+    drink_box(prod);
+  }
+  set_categories ();
+  update_language();
+}
+
 // We don't update the view the first time until the document is ready
 // loading.
-//
 $(document).ready(function() {
   intialize_view();
 })
-=======
-  if (language==true){// English
-     document.getElementById("languageId").src="./resources/images/swedish.png";
-     language=!language;
-  }else{
-    document.getElementById("languageId").src="./resources/images/english.png";
-    language=!language;
-  }// Swedish
-}
