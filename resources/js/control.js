32c297d111a6b693d4d39adf49637b7c593e6307
BorisUppsala <79930033+BorisUppsala@users.noreply.github.com>
2021-03-15 18:46:12 +0100

    Update control.js

--- resources/js/control.js
+++ resources/js/control.js
@@ -1,3 +1,22 @@
+
+//============================= Drag and Drop ================================
+
+      function allowDrop(ev) {
+            ev.preventDefault();
+        }
+
+        function drag(ev) {
+            ev.dataTransfer.setData("text", ev.target.id);
+        }
+
+        function drop(ev, el) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("text");
+            ev.target.appendChild(document.getElementById(data));
+        }
+
+
+
 //============================== Switch language ===============================
 function switchLanguage () {
   if (language=='en'){// English
